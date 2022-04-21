69be73190cc11d7d15e45fb71c68a964e2b35a4c
matheusGomes-del <matheus.gomes.santos@outlook.com>
2022-04-20 22:24:55 -0300

    adiciona primeiro requisito

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Profile from './pages/Profile';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Router>
+        <p>TrybeTunes</p>
+        <Route path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="*" component={ NotFound } />
+      </Router>
+    );
   }
-  
 }
 
 export default App;
